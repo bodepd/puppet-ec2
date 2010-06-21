ba4730051af24e5c5a35ef9fa7520357c25aea68
Dan Bode <dan@puppetlabs.com>
2010-06-21 09:32:24 -0700

    commented out mem/cpu/arch code. commented out ensurable block.

--- lib/puppet/type/ec2.rb
+++ lib/puppet/type/ec2.rb
@@ -1,6 +1,5 @@
 #
 # blah!
-#
 # this has mainly been written to facilitate ec2 instance provisioning with puppet.
 #
 # I am sure that more attributes will have to be added.
@@ -14,18 +13,59 @@ Puppet::Type.newtype(:ec2) do
   #    * running (alias for present),
   #    * absent - terminate (is there anything more brutal than terminate?)
   # 
+
   ensurable
+#  ensurable do
+#
+#  newvalue(:present, :event => :created) do
+#    provider.create
+#  end
+#  aliasvalue(:running, :present)
+#
+#  newvalue(:absent, :event => :terminated) do
+#    provider.destroy
+# end
+#  aliasvalue(:terminated, :absent)
+#
+#  newvalue(:stopped, :event => :stopped) do
+#    provider.stop
+#  end
+#
+#  def retrieve
+#    if provider
+#      provider.get('ensure')
+#    else
+#      puts 'provider does not exist, I think this is like absent'
+#      'absent'
+#    end
+#  end
+#
+#  def insync?(is)
+#    @should.each do |value|
+#      case value 
+#      when :present, :running
+#        return [:pending, :running].include?(is)
+#      when :absent, :terminated
+#        return [:'shutting-down', :terminated, :absent].include?(is)
+#      when :stopped
+#        return ![:pending, :running].include?(is)
+#      when is
+#        puts 'is there another case'
+#      else
+#        puts 'else'
+#      end
+#    end
+#  end
+#
+#  end
 
   def refresh
     provider.restart
   end
 
   # you cannot only manage security groups on create instance. 
-  newparam(:name) do
+  newparam(:name, :namevar => true) do
     desc "unique id for a machine"
-    isnamevar
-    validate do
-    end
   end
 
   #
@@ -44,20 +84,19 @@ Puppet::Type.newtype(:ec2) do
     desc "owner of node, this will refer to the account responsible for
     provisioning for cloud services, this may also be required
     "
-#    munge do
-#      'bob'
-#    end
+    isrequired
   end
 
   # this is a feature only required for cloud usage
   newparam(:password) do
     desc "password used to connect to cloud providers"
-    # this is only required for cloud providers
+    isrequired
   end
 
   newparam(:image) do 
     desc "base image used during installation"
     # this is only required when we create
+    isrequired
   end
 
   newparam(:desc) do
@@ -70,10 +109,10 @@ Puppet::Type.newtype(:ec2) do
   end
 
   newparam(:type) do
-	VALID_TYPES = [ 'm1.small', 'm1.large', 'm1.xlarge',
+	  VALID_TYPES = [ 'm1.small', 'm1.large', 'm1.xlarge',
         'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge', 'c1.medium', 'c1.xlarge']
         # check valid values from array
-	newvalues(*VALID_TYPES)
+	  newvalues(*VALID_TYPES)
     desc "this is how ec2 qualifies machine strength, I would rather use memory or ncpus or something else..."
     munge do |value|
       value.downcase
@@ -86,59 +125,59 @@ Puppet::Type.newtype(:ec2) do
   # this should be a property but its just not supported
   # with the ec2 ruby bindings... yet.
   #
-  newparam(:memory) do
-    validate do |mem|
-      unless mem =~ /\d+(GB|KB|MB)?/
-        raise Puppet::Error, "Unexpected memory value #{mem}"
-      end
-    end
-    munge do |mem|
-      mem.capitalize!
-      if mem =~ /(\d+)(GB|MB|KB)?/
-        mult = 1
-        if $2 == 'KB'
-          mult = 1000
-        elsif $2 == 'MB'
-          mult = 1000000
-        elsif $2 == 'GB'
-          mult = 1000000000
-        end 
-        mem.to_i*mult
-      end
-    end
-  end
+#  newparam(:memory) do
+#    validate do |mem|
+#      unless mem =~ /\d+(GB|KB|MB)?/
+#        raise Puppet::Error, "Unexpected memory value #{mem}"
+#      end
+#    end
+#    munge do |mem|
+#      mem.capitalize!
+#      if mem =~ /(\d+)(GB|MB|KB)?/
+#        mult = 1
+#        if $2 == 'KB'
+#          mult = 1000
+#        elsif $2 == 'MB'
+#          mult = 1000000
+#        elsif $2 == 'GB'
+#          mult = 1000000000
+#        end 
+#        mem.to_i*mult
+#      end
+#    end
+#  end
 
   #
   # this should be a property eventually.
   #
-  newparam(:cpu) do
-    validate do |cpu|
-      unless cpu =~ /\d+/
-        raise Puppet::Error, "Expected int for cpu, got #{cpu}" 
-      end
-    end
-    munge do |cpu|
-      cpu.to_i
-    end
-    defaultto '1'
-  end
+#  newparam(:cpu) do
+#    validate do |cpu|
+#      unless cpu =~ /\d+/
+#        raise Puppet::Error, "Expected int for cpu, got #{cpu}" 
+#      end
+#    end
+#    munge do |cpu|
+#      cpu.to_i
+#    end
+#    defaultto '1'
+#  end
 
   #
   #  this should probably be a property eventually.
   #
-  newparam(:arch) do
-    validate do |arch|
-      unless arch =~ /(i386|x86_64)/
-        raise Puppet::Error, "Unsupported arch #{arch}"
-      end
-    end
-    defaultto 'i386'
-  end 
+#  newparam(:arch) do
+#    validate do |arch|
+#      unless arch =~ /(i386|x86_64)/
+#        raise Puppet::Error, "Unsupported arch #{arch}"
+#      end
+#    en
+#    defaultto 'i386'
+#  end 
 
-  newparam(:sync) do
-    desc 'how obsessed should we be with synchronizing in general, this is not impleneted as of yet.'
-    defaultto true
-  end
+#  newparam(:sync) do
+#    desc 'how obsessed should we be with synchronizing in general, this is not impleneted as of yet.'
+#    defaultto true
+#  end
  
 #  newparam(:group) do
 #    desc "node group that will be used for parameterization"
