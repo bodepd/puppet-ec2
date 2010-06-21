36ddc23ab6e531ad35f05feea5d176d6999dbf26
Dan Bode <dan@puppetlabs.com>
2010-06-21 09:36:16 -0700

    this is a full rewrite although there are no functional changes. This rewrite is based on recommendations from Luke.

--- lib/puppet/provider/ec2/ec2.rb
+++ lib/puppet/provider/ec2/ec2.rb
@@ -3,250 +3,130 @@
 #
 # even though, if you use this code, you'll actually have to make it do something useful.
 #
-require 'AWS'
-
+require 'aws'
 Puppet::Type.type(:ec2).provide(:ec2) do
 
-  @doc = "
-This is the provider for noder that builds nodes on ec2
-
-  there will likely be a virtual shit-ton (VST) of documentation added later
-"
-
-  #
-  # get connection for ec2
-  # this would be so much easier if I didnt have to support multple user accounts.
-  #
-  def self.ec2_connection(username, password)
-    @ec2 ||= {}
+  # configure ec2 connection
+  def self.new_ec2(username, password)
     opts = {
       :access_key_id => username,
       :secret_access_key => password
     }
-    @ec2[username] ||= AWS::EC2::Base.new(opts)
+    AWS::EC2::Base.new(opts)
   end
 
-
-  # for each ec2 instance
-  #   -  build up the group hash
-  def self.id_instances()
-    # all instances
-    @instances ||= {}
-    @ec2.each do |k, v|
-      instance_per_group(v)  
-    end
-  end
-
-  #
-  # parse a single ec2 connection 
-  # build up a hash of group names -> instance ids
-  #
-  def self.instance_per_group(ec2)
-    reservations = ec2.describe_instances.reservationSet
-    if reservations
-      reservations.item.each do |instance|
-        # ignore all groups that are not prefixed with PUPPET_ (namevars)
-        namevars = instance.groupSet.item.select do |item|
-          item.groupId =~ /^PUPPET_/
-        end
-        if namevars.size > 1 || instance.instancesSet.item.size > 1
-          raise Exception, 'Puppet only allows 1 instance per group'
-        end
-        namevars.each do |group|
-          gid = group.groupId
-          instance.instancesSet.item.each do |instance|
-            state = instance.instanceState.name
-            if state == 'terminated' || state == 'shutting-down' 
-              # ignoring terminating states
-            else 
-              if @instances.has_key?(gid)
-                raise Exception, "duplicate group #{gid}, was #{@instances[gid]} is #{instance.instanceId}"
-              end
-              @instances[gid] = {
-                :instance_id => instance.instanceId,
-                :state => state
-              }
-            end
+  # get all of the instances for a certain user.
+  def self.user_instances(ec2)
+    return unless reservations = ec2.describe_instances.reservationSet
+    results = {}
+    reservations.item.each do |instance|
+    # ignore all groups that are not prefixed with PUPPET_ (namevars)
+      namevars = instance.groupSet.item.select do |item|
+        item.groupId =~ /^PUPPET_/
+      end
+      if namevars.size > 1 || instance.instancesSet.item.size > 1
+        raise Exception, 'Puppet only allows 1 instance per group'
+      end
+      gid = namevars.shift.groupId
+      instance.instancesSet.item.each do |instance|
+        state = instance.instanceState.name
+        if state == 'terminated' || state == 'shutting-down'
+        # ignoring terminating states
+        else
+          if results.has_key?(gid)
+            raise Exception, "duplicate group: #{gid}, was #{results[gid]} is #{instance.instanceId}"
           end
+          results[gid] = new(
+            :name => gid,
+            :instance_id => instance.instanceId,
+            :ensure => state.to_sym
+          )
         end
       end
-    end 
-    puts @instances.to_yaml
+    end
+    results
+  end
+  
+
+  #
+  # this is the method that is called for ralsh query and purging
+  #
+  # I could query based ENVIRONMENT variabels, just query the ec2 instances
+  # for a certain user/password combo
+  #
+  #
+  def self.instances
+   raise Puppet::Error, 'instances does not make sense for ec2, must know a username and pw'
   end
 
   #
-  # get the ec2 connection by username
+  # figure out which instances currently exist!
   #
-  def self.ec2(username)
-    @ec2[username]
-  end
-
-  #
-  # return the actual ec2 instance id for an instance identified by
-  # its membership to a security group
-  #
-  def self.instance_id(group)
-    if @instances[group]
-      @instances[group][:instance_id]
-    else
-      nil
+  # this is a little complicated... for performance reasons.
+  # I wanted to do a single operation to calculate all of the 
+  # instance information
+  def self.prefetch(resources)
+    resources_by_user = {}
+    users = {}
+    resources.each do |name, resource|
+      resources_by_user[resource[:user]] ||= []
+      resources_by_user[resource[:user]] << resource
+      users[resource[:user]] ||= resource[:password]
+    end
+    users.each do |user, password|
+      resources = resources_by_user[user]
+      ec2 = self.new_ec2(user, password)
+      instances = self.user_instances(ec2)
+      resources_by_user[user].each do |res|
+        if instances and instances[res[:name]]
+          res.provider = instances[res[:name]]
+        end
+      end
     end
   end
 
-  #
-  # returns the current state for an instance.
-  #
-  def self.instance_state(group)
-    if @instances[group]
-      @instances[group][:state]
-    else
-      nil
-    end
-  end
-
-  # hash the resources per user
-  def self.prefetch(resources) 
-#puts resources.to_yaml
-    resources.each do |k,v|
-      ec2_connection(v[:user], v[:password])
-    end
-    id_instances()
-  end
-#
-#  def self.get_type(mem, cpu, arch='i386')
-#    if arch == 'i386'
-#      if mem > 1700000000
-#        raise Exception, "Maximum memory is =~1.7GB with i386"
-#      else
-#        if cpu == 1
-#          # memory = 1.7
-#          # EC2 compute units = 1
-#          'm1.small'
-#        elsif cpu <= 5
-#          'c1.medium'
-#        else
-#          raise Exception, "Maximum cpu units for ec2 with i386 is 5, not #{cpu}"
-#        end
-#      end
-#    elsif arch == 'x86_64'
-#      if mem <= 7500000000
-#        if cpu <= 4
-#          'm1.large'
-#        else
-#          # need something more memory intensive
-#        end
-#      elsif mem <= 15000000000
-#        if cpu <= 8
-#          'm1.xlarge'
-#        end
-#      elsif mem <= 17100000000
-#        if cpu <= 6.5
-#          'm2.xlarge'
-#        elsif cpu <= 20
-#          'c1.xlarge'
-#        else
-#          raise Exception, 'cannot support < 17.1GB RAM with more than 20 cpu units'
-#        end
-#      elsif mem <= 34200000000
-#
-#        'm2.2xlarge'
-#      elsif mem <= 68400000000
-#        if cpu <= 26
-#          'm2.4xlarge'
-#        else
-#          raise Exception, "ec2 does not support more than 26 CPU units"
-#        end
-#      else
-#        raise Exception, "ec2 does not support more than 68.4GB RAM"
-#      end
-#    end
-    #if memory < 2000000000 && cpu == 1 && arch == 'i386'
-    #  'm1.small'
-    #end
-#  end
-
+  # essentially doing a provider munge to prefix
+  # identifying security groups with PUPPET
+  # this is required to simply allow other groups.
   def initialize(resource)
-    # essentially doing a provider munge to prefix
-    # identifying security groups with PUPPET
-    # this is required to simply allow other groups.
-    resource[:name] = "PUPPET_#{resource[:name]}"
+    unless resource.instance_of?(Hash)
+      resource[:name] = "PUPPET_#{resource[:name]}"
+    end
     super(resource)
   end
 
-  #
-  # ensures that the EC2 instance exists and is running
-  #
-  def create 
-    ec2 =  self.class.ec2(@resource.value(:user))
+  # create namevar security group and start instance.
+  def create
+    ec2 = self.class.new_ec2(@resource.value(:user), @resource.value(:password))
     group = @resource.value(:name)
-    # create the new security group
     begin
       ec2.describe_security_groups({:group_name => group})
     rescue Exception => e
-      unless self.class.instance_state(group)
-        ec2.create_security_group(
-          { 
-            :group_name => group, 
-            :group_description => @resource.value(:desc)
-          } 
-        )
-      end
+      ec2.create_security_group({ 
+        :group_name => group,
+        :group_description => @resource.value(:desc)
+      })
     end
     # if instance in that security group exists, start it
     # otherwise just create a new instance 
     ec2.run_instances(
-      {
-        :image_id => @resource.value(:image),
-        # security groups
-        :security_group => group,
-        :instance_type => @resource.value(:type),
-      } 
-    )
+    { :image_id => @resource.value(:image),
+      # security groups
+      :security_group => group,
+      :instance_type => @resource.value(:type)
+    })
   end
-  
 
-  # determine if an ec2 instance exists
+  # see if the instance in in the running or pending state.
   def exists?
-    # if we have a security group with our name and it has at least one member
-    state = self.class.instance_state(@resource.value(:name))
-    state == 'running' || state == 'pending'
+    @property_hash and [:running, :pending].include?(@property_hash[:ensure])
   end
 
-  # destory an ec2 instance if it exists
+  # terminate the ec2 instance
   def destroy 
-    group = @resource.value(:name)
-    ec2 =  self.class.ec2(@resource.value(:user))
-    instance = self.class.instance_id(group)
-    ec2.terminate_instances({:instance_id => instance})       
-    unless group == 'default' 
-      ec2.delete_security_group({:group_name => group})
-    else
-      puts 'cannot remove default'
-    end
+    ec2 = self.class.new_ec2(@resource.value(:user), @resource.value(:password))
+    ec2.terminate_instances({:instance_id => @property_hash[:instance_id]})
+    ec2.delete_security_group({:group_name => @resource.value(:name)})
   end
-
-  # reboot the instance when we receive a signal
-  def restart
-    group = @resource.value(:name)
-    instance = self.class.instance_id(group)
-    ec2 = self.class.ec2(@resource.value(:user))
-puts 'rebooting'
-    puts ec2.reboot_instances({:instance_id => instance}).to_yaml
-    puts ec2.describe_instances({:instance_id => instance}).to_yaml
-   # state = self.class.instance_state(@resource.value(:name))
-   # state == 'running' || state == 'pending'
-  end
-
-#  private
-
-#    def connect
-#      opts = {
-#        :access_key_id => ,
-#        :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
-#      }
-#
-#      @ec2 =  
-#    end
-    
 
 end
